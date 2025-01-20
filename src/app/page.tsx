940a7e3d328ea62c23602ce19cdc9bf4f1e39b1b
NickNedilko <31307217+NickNedilko@users.noreply.github.com>
2025-01-20 14:49:55 +0200

    first commit
    initialize project, add image to home page

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex items-center h-screen relative bg-cover bg-[url('/assets/bg-explosion.png')]">
+      <div className="absolute right-10 md:right-40 bottom-0 z-[10]">
+        <Image
+          src='/assets/MoustacheMan.png'
+          alt='MoustacheMan'
+          width={560}
+          height={560}
+          className="w-[300px] h-[300px] z-1 md:h-[560px] md:w-[560px] "
+        />
+      </div>
+      <div>
+        <Image
+          src='/assets/top-left-img.png'
+          alt='paint'
+          width={230}
+          height={230}
+          className="absolute left-0 top-0"
+        />
+      </div>
+    </main>
+  );
+}
